fafd5010799d6f0f98c4cdc00c964c8a274ae6bb
Diamant <git@diamant.dev>
2021-06-20 00:25:16 +0200

    💚 Meta tags and footer.

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,19 @@ pokemonJS.getAll().then((pokemon: [string]) => {
     img.src = url.other['official-artwork'].front_default;
     const icon = document.querySelector('.icon') as HTMLLinkElement;
     icon.href = url.front_default;
+    const a =
+      rdmPoke.startsWith('a') ||
+      rdmPoke.startsWith('e') ||
+      rdmPoke.startsWith('i') ||
+      rdmPoke.startsWith('o') ||
+      rdmPoke.startsWith('u')
+        ? 'an'
+        : 'a';
+    const shareURL = `https://twitter.com/intent/tweet?text=Today,I'm ${a} ${rdmPoke}, and you? Check here:&hashtags=WhatPokemonAreYouToday,Pokemon&url=https://wpart.diams.app`;
+    const aP = document.querySelector('#a') as HTMLSpanElement;
+    aP.innerText = a;
+    const tweetBtn = document.querySelector('#tweet') as HTMLLinkElement;
+    tweetBtn.href = shareURL;
+    tweetBtn.removeAttribute('hidden');
   });
 });
